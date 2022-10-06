07c2b7cc091aafd68491ade69735acc6a809d7be
jake <palatable7@naver.com>
2022-10-06 14:07:49 +0900

    Initial Commit

---
+++ ExPreventCapture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExPreventCapture
+//
+//  Created by 김종권 on 2022/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
