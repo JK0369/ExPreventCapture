90096c32ff7fcd9ed53ca955194c176b6447c756
jake <palatable7@naver.com>
2022-10-07 09:50:14 +0900

    Add sample

--- ExPreventCapture/ViewController.swift
+++ ExPreventCapture/ViewController.swift
@@ -21,6 +21,17 @@ class ViewController: UIViewController {
     label.translatesAutoresizingMaskIntoConstraints = false
     return label
   }()
+  private let secureTextField: UITextField = {
+    let textField = UITextField()
+    textField.placeholder = "텍스트 필드 placeholder"
+    textField.isSecureTextEntry = true
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    textField.layer.cornerRadius = 8.0
+    textField.layer.masksToBounds = true
+    textField.layer.borderColor = UIColor.gray.cgColor
+    textField.layer.borderWidth = 1.0
+    return textField
+  }()
   private let tableView: UITableView = {
     let tableView = UITableView()
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -34,13 +45,19 @@ class ViewController: UIViewController {
     tableView.dataSource = self
     tableView.makeSecure()
     
-    [label, tableView]
+    [label, secureTextField, tableView]
       .forEach(view.addSubview(_:))
     
     NSLayoutConstraint.activate([
       label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 32),
       label.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -32),
       label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24)
+    ])
+    NSLayoutConstraint.activate([
+      secureTextField.leftAnchor.constraint(equalTo: label.leftAnchor),
+      secureTextField.rightAnchor.constraint(equalTo: label.rightAnchor),
+      secureTextField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 16),
+      secureTextField.heightAnchor.constraint(equalToConstant: 45)
     ])
     NSLayoutConstraint.activate([
       tableView.leftAnchor.constraint(equalTo: label.leftAnchor),
